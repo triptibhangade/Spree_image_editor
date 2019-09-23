8c8853135bca50bc410fad35671a6fe183727229
User4 <tripti.bhangade@gemsessence.com>
2019-09-23 13:45:59 +0530

    add spree_templates table by migration

---
+++ db/migrate/20190923081536_add_spree_templates.rb
@@ -0,0 +1,16 @@
+class AddSpreeTemplates < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spree_templates do |t|
+      t.string :name
+      t.string :description
+      t.decimal :width
+      t.decimal :height
+      t.decimal :scale, precision: 5, scale: 2
+      t.string :edges
+      t.string :slug
+      t.datetime :deleted_at
+      
+      t.timestamps
+    end
+  end
+end
